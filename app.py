d684ea601f9f7a55c42bf93a5a4a0f711bb2fc28
jga <javieralumno1asir@gmail.com>
2023-08-03 10:37:28 +0200

    Adding sql database

--- app.py
+++ app.py
@@ -1,32 +1,55 @@
 import os
-
-from flask import (Flask, redirect, render_template, request,
-                   send_from_directory, url_for)
+import pyodbc
+from flask import Flask, redirect, render_template, request, send_from_directory, url_for
 
 app = Flask(__name__)
 
+# Configura la conexión a la base de datos
+config = {
+    'driver': '{ODBC Driver 17 for SQL Server}',
+    'server': 'jga.database.windows.net',
+    'database': 'coches',
+    'uid': 'jga',
+    'pwd': '1234!Strong',
+    'Encrypt': 'yes',
+    'TrustServerCertificate': 'no',
+    'Connection Timeout': 30
+}
+
+# Función para obtener una conexión a la base de datos
+def get_db_connection():
+    conn_str = ";".join([f"{key}={value}" for key, value in config.items()])
+    return pyodbc.connect(conn_str)
 
 @app.route('/')
 def index():
-   print('Request for index page received')
-   return render_template('index.html')
+    print('Request for index page received')
+    return render_template('index.html')
+
+@app.route('/coches')
+def coches():
+    connection = get_db_connection()
+    cursor = connection.cursor()
+    cursor.execute('SELECT * FROM coches')
+    data = cursor.fetchall()
+    cursor.close()
+    connection.close()
+    return render_template('coches.html', data=data)
 
 @app.route('/favicon.ico')
 def favicon():
-    return send_from_directory(os.path.join(app.root_path, 'static'),
-                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
+    return send_from_directory(os.path.join(app.root_path, 'static'), 'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
 @app.route('/hello', methods=['POST'])
 def hello():
-   name = request.form.get('name')
+    name = request.form.get('name')
 
-   if name:
-       print('Request for hello page received with name=%s' % name)
-       return render_template('hello.html', name = name)
-   else:
-       print('Request for hello page received with no name or blank name -- redirecting')
-       return redirect(url_for('index'))
-
+    if name:
+        print('Request for hello page received with name=%s' % name)
+        return render_template('hello.html', name=name)
+    else:
+        print('Request for hello page received with no name or blank name -- redirecting')
+        return redirect(url_for('index'))
 
 if __name__ == '__main__':
-   app.run()
+    app.run()
